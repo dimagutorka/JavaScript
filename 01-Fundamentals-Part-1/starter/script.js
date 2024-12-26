170e6bc5c81ef8b610d6d3d9fde813c18e6feecd
dimagutorka <dgutorka@gmail.com>
2024-12-26 21:33:28 +0200

    First lesson, basics

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+const myCountry = "Ukraine";
+const myContinent = "Europe";
+const isIsland = false
+const language = 'UA';
+
+let countryPopulation = 10000000;
+
+console.log(myContinent)
+console.log(isIsland)
+console.log(myCountry)
+console.log(countryPopulation)
+console.log(language)
+
+console.log(typeof isIsland)
+console.log(typeof countryPopulation)
+console.log(typeof language)
+
